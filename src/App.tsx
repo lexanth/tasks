816a1581b48394afa388b0cfac67c1911cb93152
Alex Anthony <alex.anthony28991@gmail.com>
2019-06-09 09:33:33 +0200

    Add custom style to scrollbars
    Just using CSS as we know we'll always be in electron

--- src/App.tsx
+++ src/App.tsx
@@ -9,56 +9,70 @@ import { lighten } from 'polished'
 import moize from 'moize'
 
 const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-  padding: 0;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
 
-body, input {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  body, input {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-code, pre, textarea {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
+  code, pre, textarea {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
 
-h4, h1, p {
-  margin: 0;
-  padding: 0;
-}
-a {
-  color: inherit;
-}
+  h4, h1, p {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: inherit;
+  }
 
-    .Resizer {
-        background-color: ${props => props.theme.primary.dark};
-        z-index: 1;
-    }
+  .Resizer {
+      background-color: ${props => props.theme.primary.dark};
+      z-index: 1;
+      transition: all 0.1s ease;
+  }
 
-     .Resizer:hover {
-        transition: all 0.1s ease;
-        background-color: ${props => lighten(0.1, props.theme.primary.dark)};
-    }
+    .Resizer:hover {
+      background-color: ${props => lighten(0.1, props.theme.primary.dark)};
+  }
 
-    .Resizer.vertical {
-        width: 10px;
-        margin: 0;
-        cursor: col-resize;
-    }
-    .Resizer.disabled {
-      cursor: not-allowed;
-    }
-    .Resizer.disabled:hover {
-      border-color: transparent;
-    }
-    #root {
-      height: 100%;
-    }
+  .Resizer.vertical {
+      width: 10px;
+      margin: 0;
+      cursor: col-resize;
+  }
+  .Resizer.disabled {
+    cursor: not-allowed;
+  }
+  .Resizer.disabled:hover {
+    border-color: transparent;
+  }
+  #root {
+    height: 100%;
+  }
+  ::-webkit-scrollbar { 
+    width: 8px;
+    height: 8px;
+  }
+  /* Handle */ 
+  ::-webkit-scrollbar-thumb { 
+    -webkit-border-radius: 10px; 
+    border-radius: 10px; 
+    background: ${props => props.theme.primary.dark};
+    transition: all 0.1s ease;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: ${props => lighten(0.1, props.theme.primary.dark)};
+  }
 `
 
 type State = {
