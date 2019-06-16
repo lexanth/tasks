8e4d1f9e37e549966efd7db2ecc6575bae78a3b0
Alex Anthony <alex.anthony28991@gmail.com>
2019-06-16 16:26:33 +0200

    Bump version for release

--- public/electron.js
+++ public/electron.js
@@ -30,7 +30,7 @@ function createWindow() {
   if (process.platform !== 'win32') {
     app.setAboutPanelOptions({
       applicationName: 'Tasks',
-      applicationVersion: '0.1.6',
+      applicationVersion: '0.1.7',
     })
   }
 
